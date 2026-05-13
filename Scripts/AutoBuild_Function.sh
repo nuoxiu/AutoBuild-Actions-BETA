6319817825d296240978c41b927233b79c9b6967
nuoxiu <100852963+nuoxiu@users.noreply.github.com>
2026-05-13 16:47:45 +0800

    Update AutoBuild_Function.sh

--- Scripts/AutoBuild_Function.sh
+++ Scripts/AutoBuild_Function.sh
@@ -1,136 +1,88 @@
-#!/bin/bash
-# AutoBuild Module by Hyy2001 <https://github.com/Hyy2001X/AutoBuild-Actions-BETA>
-# AutoBuild Functions
-
-Firmware_Diy_Start() {
-	ECHO "[Firmware_Diy_Start] Starting ..."
-	WORK="${GITHUB_WORKSPACE}/openwrt"
-	CONFIG_TEMP="${WORK}/.config"
+Firmware_Diy_Main() {
+	ECHO "[Firmware_Diy_Main] Starting ..."
 	CD ${WORK}
-	OP_REPO="$(basename $(cut -d ':' -f1 <<< ${DEFAULT_SOURCE}))"
-	OP_AUTHOR="$(cut -d '/' -f1 <<< ${DEFAULT_SOURCE})"
-	OP_BRANCH="$(cut -d ':' -f2 <<< ${DEFAULT_SOURCE})"
-	Firmware_Diy_Core
-	[[ ${Short_Fw_Date} == true ]] && Compile_Date="$(cut -c1-8 <<< ${Compile_Date})"
-	Github="$(egrep -o 'https://github.com/.+' ${GITHUB_WORKSPACE}/.git/config | awk 'NR==1')"
-	[[ -z ${Author} || ${Author} == AUTO ]] && Author="$(cut -d "/" -f4 <<< ${Github} | awk 'NR==1')"
-	if [[ ${OP_BRANCH} =~ (master|main) ]]
+	# 确保 Scripts 和 CustomFiles 变量已定义（防止未从环境变量继承）
+	Scripts="${GITHUB_WORKSPACE}/Scripts"
+	CustomFiles="${GITHUB_WORKSPACE}/CustomFiles"
+	chmod 777 -R ${Scripts} ${CustomFiles}
+	if [[ ${AutoBuild_Features} == true ]]
 	then
-		OP_VERSION_HEAD="R$(date +%y.%m)-"
-	else
-		OP_VERSION_HEAD="R$(egrep -o "[0-9]+.[0-9]+" <<< ${OP_BRANCH} | awk 'NR==1')-"
-	fi
-	case "${OP_AUTHOR}/${OP_REPO}" in
-	coolsnowwolf/lede)
-		Version_File=package/lean/default-settings/files/zzz-default-settings
-		zzz_Default_Version="$(egrep -o "R[0-9]+\.[0-9]+\.[0-9]+" ${Version_File})"
-		OP_VERSION="${zzz_Default_Version}-${Compile_Date}"
-	;;
-	immortalwrt/immortalwrt | padavanonly/immortalwrtARM | hanwckf/immortalwrt-mt798x)
-		Version_File=package/base-files/files/etc/openwrt_release
-		OP_VERSION="${OP_VERSION_HEAD}${Compile_Date}"
-	;;
-	*)
-		OP_VERSION="${OP_VERSION_HEAD}${Compile_Date}"
-	;;
-	esac
-	while [[ -z ${x86_Test} ]]
-	do
-		x86_Test="$(egrep -o "CONFIG_TARGET.*DEVICE.*=y" ${CONFIG_TEMP} | sed -r 's/CONFIG_TARGET_(.*)_DEVICE_(.*)=y/\1/')"
-		[[ -n ${x86_Test} ]] && break
-		x86_Test="$(egrep -o "CONFIG_TARGET.*Generic=y" ${CONFIG_TEMP} | sed -r 's/CONFIG_TARGET_(.*)_Generic=y/\1/')"
-		[[ -z ${x86_Test} ]] && break
-	done
-	if [[ ${x86_Test} == x86_64 ]]
-	then
-		TARGET_PROFILE=x86_64
-	else
-		TARGET_PROFILE="$(egrep -o "CONFIG_TARGET.*DEVICE.*=y" ${CONFIG_TEMP} | sed -r 's/.*DEVICE_(.*)=y/\1/')"
-	fi
-	TARGET_BOARD="$(awk -F '[="]+' '/TARGET_BOARD/{print $2}' ${CONFIG_TEMP})"
-	TARGET_SUBTARGET="$(awk -F '[="]+' '/TARGET_SUBTARGET/{print $2}' ${CONFIG_TEMP})"
-	if [[ -z ${Fw_MFormat} || ${Fw_MFormat} == AUTO ]]
-	then
-		case "${TARGET_BOARD}" in
-		ramips | reltek | ath* | ipq* | bcm47xx | bmips | kirkwood | mediatek)
-			Fw_MFormat=bin
+		AddPackage other Hyy2001X AutoBuild-Packages master
+		echo -e "\nCONFIG_PACKAGE_luci-app-autoupdate=y" >> ${CONFIG_FILE}
+		AutoUpdate_Version=$(awk -F '=' '/Version/{print $2}' $(PKG_Finder d package AutoBuild-Packages)/autoupdate/files/bin/autoupdate | awk 'NR==1')
+		cat >> $(PKG_Finder d package AutoBuild-Packages)/autoupdate/files/etc/autoupdate/default <<EOF
+Author=${Author}
+Github=${Github}
+TARGET_PROFILE=${TARGET_PROFILE}
+TARGET_BOARD=${TARGET_BOARD}
+TARGET_SUBTARGET=${TARGET_SUBTARGET}
+TARGET_FLAG=${TARGET_FLAG}
+OP_VERSION=${OP_VERSION}
+OP_AUTHOR=${OP_AUTHOR}
+OP_REPO=${OP_REPO}
+OP_BRANCH=${OP_BRANCH}
+
+EOF
+		Copy ${CustomFiles}/Depends/tools ${BASE_FILES}/bin
+		Copy ${CustomFiles}/Depends/profile ${BASE_FILES}/etc
+		Copy ${CustomFiles}/Depends/base-files-essential ${BASE_FILES}/lib/upgrade/keep.d
+		case "${OP_AUTHOR}/${OP_REPO}" in
+		coolsnowwolf/lede)
+			Copy ${CustomFiles}/Depends/coremark.sh $(PKG_Finder d "package feeds" coremark)
+			sed -i '\/etc\/firewall.user/d;/exit 0/d' ${Version_File}
+			if [[ -n ${TARGET_FLAG} ]]
+			then
+				sed -i "s?${zzz_Default_Version}?${TARGET_FLAG} ${zzz_Default_Version} @ ${Author} [${Display_Date}]?g" ${Version_File}
+			else
+				sed -i "s?${zzz_Default_Version}?${zzz_Default_Version} @ ${Author} [${Display_Date}]?g" ${Version_File}
+			fi
 		;;
-		rockchip | x86 | bcm27xx | mxs | sunxi | zynq)
-			Fw_MFormat="$(gz_Check)"
+		immortalwrt/immortalwrt | padavanonly/immortalwrtARM | hanwckf/immortalwrt-mt798x)
+			Copy ${CustomFiles}/Depends/openwrt_release_immortalwrt ${BASE_FILES}/etc openwrt_release
+			Copy ${CustomFiles}/Depends/os-release_immortalwrt ${BASE_FILES}/usr/lib os-release
+			if [[ -n ${TARGET_FLAG} ]]
+			then
+				sed -i "s?ImmortalWrt?ImmortalWrt ${TARGET_FLAG} @ ${Author} [${Display_Date}]?g" ${Version_File}
+				sed -i "s?ImmortalWrt?ImmortalWrt ${TARGET_FLAG} @ ${Author} [${Display_Date}]?g" ${BASE_FILES}/usr/lib/os-release
+			else
+				sed -i "s?ImmortalWrt?ImmortalWrt @ ${Author} [${Display_Date}]?g" ${Version_File}
+				sed -i "s?ImmortalWrt?ImmortalWrt @ ${Author} [${Display_Date}]?g" ${BASE_FILES}/usr/lib/os-release
+			fi
 		;;
-		mvebu)
-			case "${TARGET_SUBTARGET}" in
-			cortexa53 | cortexa72)
-				Fw_MFormat="$(gz_Check)"
-			;;
-			esac
-		;;
-		octeon | oxnas | pistachio)
-			Fw_MFormat=tar
+		esac
+		sed -i "s?By?By ${Author}?g" ${CustomFiles}/Depends/banner
+		sed -i "s?Openwrt?Openwrt ${OP_VERSION} / AutoUpdate ${AutoUpdate_Version}?g" ${CustomFiles}/Depends/banner
+		if [[ -n ${Default_Title} ]]
+		then
+			if [[ -n ${TARGET_FLAG} ]]
+			then
+				sed -i "s?Powered by AutoBuild-Actions?${Default_Title} @ ${TARGET_FLAG}?g" ${CustomFiles}/Depends/banner
+			else
+				sed -i "s?Powered by AutoBuild-Actions?${Default_Title}?g" ${CustomFiles}/Depends/banner
+			fi
+		fi
+		case "${OP_AUTHOR}/${OP_REPO}" in
+		*)
+			Copy ${CustomFiles}/Depends/banner ${BASE_FILES}/etc
 		;;
 		esac
 	fi
-	[[ ${Author_URL} != false && ${Author_URL} == AUTO ]] && Author_URL="${Github}"
-	[[ ${Author_URL} == false ]] && unset Author_URL
-	if [[ ${Default_Flag} == AUTO ]]
+	if [[ -n ${Tempoary_IP} ]]
 	then
-		TARGET_FLAG=${CONFIG_FILE/${TARGET_PROFILE}-/}
-		[[ ${TARGET_FLAG} =~ ${TARGET_PROFILE} || -z ${TARGET_FLAG} || ${TARGET_FLAG} == ${CONFIG_FILE} ]] && TARGET_FLAG=Full
-	else
-		if [[ ! ${Default_Flag} =~ (\"|=|-|_|\.|\#|\|) && ${Default_Flag} =~ [a-zA-Z0-9] ]]
+		ECHO "Using Tempoary IP Address: ${Tempoary_IP} ..."
+		Default_IP="${Tempoary_IP}"
+	fi
+	if [[ -n ${Default_IP} && ${Default_IP} =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]
+	then
+		Old_IP=$(awk -F '[="]+' '/ipaddr:-/{print $3}' ${BASE_FILES}/bin/config_generate | awk 'NR==1')
+		if [[ ! ${Default_IP} == ${Old_IP} ]]
 		then
-			TARGET_FLAG="${Default_Flag}"
+			ECHO "Setting default IP Address to ${Default_IP} ..."
+			sed -i "s/${Old_IP}/${Default_IP}/g" ${BASE_FILES}/bin/config_generate
 		fi
 	fi
-	if [[ ! ${Tempoary_FLAG} =~ (\"|=|-|_|\.|\#|\|) && ${Tempoary_FLAG} =~ [a-zA-Z0-9] && ${Tempoary_FLAG} != AUTO ]]
-	then
-		TARGET_FLAG="${Tempoary_FLAG}"
-	fi
-	# 不再生成 AutoBuild_Fw 变量（避免格式问题）
-	# AutoBuild_Fw 将在后续 Process_Fw 中动态生成，无需写入环境变量
-
-	# 安全写入环境变量：只写入必要且值中不含换行符的变量
-	# 对可能包含换行符的变量进行净化
-	clean_value() {
-		echo "$1" | tr -d '\n\r' | sed 's/"/\\"/g'
-	}
-	cat >> ${GITHUB_ENV} <<EOF
-WORK=$(clean_value "${WORK}")
-CONFIG_TEMP=$(clean_value "${CONFIG_TEMP}")
-CONFIG_FILE=$(clean_value "${CONFIG_FILE}")
-AutoBuild_Features=$(clean_value "${AutoBuild_Features}")
-x86_Full_Images=$(clean_value "${x86_Full_Images}")
-CustomFiles=$(clean_value "${GITHUB_WORKSPACE}/CustomFiles")
-Scripts=$(clean_value "${GITHUB_WORKSPACE}/Scripts")
-BASE_FILES=$(clean_value "${GITHUB_WORKSPACE}/openwrt/package/base-files/files")
-FEEDS_LUCI=$(clean_value "${GITHUB_WORKSPACE}/openwrt/package/feeds/luci")
-FEEDS_PKG=$(clean_value "${GITHUB_WORKSPACE}/openwrt/package/feeds/packages")
-Default_Title=$(clean_value "${Default_Title}")
-Regex_Skip=$(clean_value "${Regex_Skip}")
-Version_File=$(clean_value "${Version_File}")
-Fw_MFormat=$(clean_value "${Fw_MFormat}")
-FEEDS_CONF=$(clean_value "${WORK}/feeds.conf.default")
-Author_URL=$(clean_value "${Author_URL}")
-Compile_Date=$(clean_value "${Compile_Date}")
-Author=$(clean_value "${Author}")
-Github=$(clean_value "${Github}")
-TARGET_PROFILE=$(clean_value "${TARGET_PROFILE}")
-TARGET_BOARD=$(clean_value "${TARGET_BOARD}")
-TARGET_SUBTARGET=$(clean_value "${TARGET_SUBTARGET}")
-TARGET_FLAG=$(clean_value "${TARGET_FLAG}")
-OP_AUTHOR=$(clean_value "${OP_AUTHOR}")
-OP_REPO=$(clean_value "${OP_REPO}")
-OP_BRANCH=$(clean_value "${OP_BRANCH}")
-EOF
-	# 注意：没有写入 OP_VERSION 和 AutoBuild_Fw，避免格式问题
-	source ${GITHUB_ENV}
-
-	# 打印变量列表（用于调试）
-	echo -e "### VARIABLE LIST ###"
-	cat ${GITHUB_ENV}
-	echo ""
-
-	ECHO "[Firmware_Diy_Start] Done"
+ 	echo -e "### VARIABLE LIST ###\n$(cat ${GITHUB_ENV})\n"
+	ECHO "[Firmware_Diy_Main] Done"
 }
 
 Firmware_Diy_Main() {
