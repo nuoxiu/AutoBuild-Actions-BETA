f98b122b7b335b40038ffd431aa85387d222df60
User <user@example.com>
2026-05-16 17:40:22 +0800

    优化 Newifi D2 编译配置: 使用 20230609 分支, 添加硬件加速和完整插件支持

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -1,80 +1,133 @@
 #!/bin/bash
-# AutoBuild Module by Hyy2001 <https://github.com/Hyy2001X/AutoBuild-Actions-BETA>
-# AutoBuild DiyScript
+# AutoBuild Module - 优化版 by nuoxiu
+# 适配 Newifi D2 + 2023-06-09 分支
 
 Firmware_Diy_Core() {
-
-	# 请在该函数内按需修改变量设置, 使用 case 语句控制不同预设变量的设置
-	
-	# 可用预设变量
-	# ${OP_AUTHOR}			OpenWrt 源码作者
-	# ${OP_REPO}			OpenWrt 仓库名称
-	# ${OP_BRANCH}			OpenWrt 源码分支
-	# ${CONFIG_FILE}		配置文件
-	
 	Author=AUTO
-	# 作者名称, AUTO: [自动识别]
-	
 	Author_URL=AUTO
-	# 自定义作者网站或域名, AUTO: [自动识别]
-	
 	Default_Flag=AUTO
-	# 固件标签 (名称后缀), 适用不同配置文件, AUTO: [自动识别]
-	
 	Default_IP="192.168.1.1"
-	# 固件 IP 地址
-	
 	Default_Title="Powered by AutoBuild-Actions"
-	# 固件终端首页显示的额外信息
-	
 	Short_Fw_Date=true
-	# 简短的固件日期, true: [20210601]; false: [202106012359]
-	
 	x86_Full_Images=false
-	# 额外上传已检测到的 x86 虚拟磁盘镜像, true: [上传]; false: [不上传]
-	
 	Fw_MFormat=AUTO
-	# 自定义固件格式, AUTO: [自动识别]
-	
 	Regex_Skip="packages|buildinfo|sha256sums|manifest|kernel|rootfs|factory|itb|profile|ext4|json"
-	# 输出固件时丢弃包含该内容的固件/文件
-	
 	AutoBuild_Features=true
-	# 添加 AutoBuild 固件特性, true: [开启]; false: [关闭]
-	
 	AutoBuild_Features_Patch=true
 	AutoBuild_Features_Kconfig=false
 }
 
 Firmware_Diy() {
-
-	# 请在该函数内定制固件
-
-	# 可用预设变量, 其他可用变量请参考运行日志
-	# ${OP_AUTHOR}			OpenWrt 源码作者
-	# ${OP_REPO}			OpenWrt 仓库名称
-	# ${OP_BRANCH}			OpenWrt 源码分支
-	# ${TARGET_PROFILE}		设备名称
-	# ${TARGET_BOARD}		设备架构
-	# ${TARGET_FLAG}		固件名称后缀
-	# ${CONFIG_FILE}		配置文件
-
-	# ${CustomFiles}		仓库中的 /CustomFiles 绝对路径
-	# ${Scripts}			仓库中的 /Scripts 绝对路径
-
-	# ${WORK}				OpenWrt 源码目录
-	# ${FEEDS_CONF}			OpenWrt 源码目录下的 feeds.conf.default 文件
-	# ${FEEDS_LUCI}			OpenWrt 源码目录下的 package/feeds/luci 目录
-	# ${FEEDS_PKG}			OpenWrt 源码目录下的 package/feeds/packages 目录
-	# ${BASE_FILES}			OpenWrt 源码目录下的 package/base-files/files 目录
-
-	# AddPackage <package_path> <git_user> <git_repo> <git_branch>
-	# ClashDL <platform> <core_type> [dev/tun/meta]
-	# ReleaseDL <release_url> <file> <target_path>
-	# Copy <cp_from> <cp_to > <rename>
-	# merge_package <git_branch> <git_repo_url> <package_path> <target_path>..
-	
+	# 使用 20230609 分支
 	case "${OP_AUTHOR}/${OP_REPO}:${OP_BRANCH}" in
+	coolsnowwolf/lede:20230609)
+		cat >> ${Version_File} <<EOF
+sed -i '/check_signature/d' /etc/opkg.conf
+if [ -z "\$(grep "REDIRECT --to-ports 53" /etc/firewall.user 2> /dev/null)" ]
+then
+	echo '# iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 53' >> /etc/firewall.user
+	echo '# iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 53' >> /etc/firewall.user
+	echo '# [ -n "\$(command -v ip6tables)" ] && ip6tables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 53' >> /etc/firewall.user
+	echo '# [ -n "\$(command -v ip6tables)" ] && ip6tables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 53' >> /etc/firewall.user
+	echo 'iptables -t mangle -A PREROUTING -i pppoe -p icmp --icmp-type destination-unreachable -j DROP' >> /etc/firewall.user
+	echo 'iptables -t mangle -A PREROUTING -i pppoe -p tcp -m tcp --tcp-flags ACK,RST RST -j DROP' >> /etc/firewall.user
+	echo 'iptables -t mangle -A PREROUTING -i pppoe -p tcp -m tcp --tcp-flags PSH,FIN PSH,FIN -j DROP' >> /etc/firewall.user
+	echo '[ -n "\$(command -v ip6tables)" ] && ip6tables -t mangle -A PREROUTING -i pppoe -p tcp -m tcp --tcp-flags PSH,FIN PSH,FIN -j DROP' >> /etc/firewall.user
+	echo '[ -n "\$(command -v ip6tables)" ] && ip6tables -t mangle -A PREROUTING -i pppoe -p ipv6-icmp --icmpv6-type destination-unreachable -j DROP' >> /etc/firewall.user
+	echo '[ -n "\$(command -v ip6tables)" ] && ip6tables -t mangle -A PREROUTING -i pppoe -p tcp -m tcp --tcp-flags ACK,RST RST -j DROP' >> /etc/firewall.user
+fi
+exit 0
+EOF
+		# 移除无用的主题
+		rm -r ${FEEDS_LUCI}/luci-theme-argon* 2>/dev/null || true
+		
+		# 添加 OpenClash
+		AddPackage other vernesong OpenClash dev
+		
+		# 添加 argon 主题 (新版本)
+		AddPackage other jerrykuku luci-app-argon-config master
+		AddPackage themes jerrykuku luci-theme-argon 18.06
+		
+		# 添加 mosdns (v5)
+		AddPackage other sbwml luci-app-mosdns v5-lua
+		
+		# 添加 msd_lite (多播代理)
+		AddPackage msd_lite ximiTech luci-app-msd_lite main
+		AddPackage msd_lite ximiTech msd_lite main
+		
+		# 添加 SmartDNS
+		AddPackage other pymumu luci-app-smartdns master
+		
+		# 添加 DDNS
+		AddPackage other openwrt-packages luci-app-ddns master
+		
+		# 添加文件管理 (FileBrowser)
+		AddPackage other 99max/luci-app-filebrowser main
+		
+		# 添加 Samba4
+		AddPackage other openwrt-packages luci-app-samba4 master
+		
+		# 添加 VSFTPD
+		AddPackage other openwrt-packages luci-app-vsftpd master
+		
+		# 添加 ZeroTier
+		AddPackage other openwrt-packages luci-app-zerotier master
+		
+		# 添加 WOL
+		AddPackage other openwrt-packages luci-app-wol master
+		
+		# 添加 UPnP
+		AddPackage other openwrt-packages luci-app-upnp master
+		
+		# 添加 TurboACC 加速
+		AddPackage other jerrykuku luci-app-turboacc master
+		
+		# 添加 SQM QoS
+		AddPackage other openwrt-packages luci-app-sqm master
+		
+		# 添加广告过滤 (AdGuard Home)
+		AddPackage other openwrt-packages luci-app-adguardhome master
+		
+		# 添加微信推送 (ServerChan)
+		AddPackage other tty228 luci-app-serverchan master
+		
+		# 移除冲突的包
+		rm -r ${FEEDS_PKG}/mosdns 2>/dev/null || true
+		rm -r ${FEEDS_LUCI}/luci-app-mosdns 2>/dev/null || true
+		rm -r ${FEEDS_PKG}/curl 2>/dev/null || true
+		rm -r ${FEEDS_PKG}/msd_lite 2>/dev/null || true
+		
+		# 复制自定义 curl
+		Copy ${CustomFiles}/curl ${FEEDS_PKG}
+		
+		# 针对 ramips 架构的优化
+		case "${TARGET_BOARD}" in
+		ramips)
+			sed -i "/DEVICE_COMPAT_VERSION := 1.1/d" target/linux/ramips/image/mt7621.mk
+			Copy ${CustomFiles}/Depends/automount $(PKG_Finder d "package" automount)/files 15-automount
+		;;
+		esac
+
+		# 针对特定配置下载 Clash 核心
+		case "${CONFIG_FILE}" in
+		d-team_newifi-d2-Clash | xiaoyu_xy-c5-Clash)
+			ClashDL mipsle-hardfloat tun
+		;;
+		esac
+		
+		# 针对特定设备复制配置
+		case "${TARGET_PROFILE}" in
+		d-team_newifi-d2)
+			Copy ${CustomFiles}/${TARGET_PROFILE}_system ${BASE_FILES}/etc/config system
+		;;
+		xiaomi_redmi-router-ax6s)
+			AddPackage passwall-depends Openwrt-Passwall openwrt-passwall-packages main
+			AddPackage passwall-luci Openwrt-Passwall openwrt-passwall main
+		;;
+		esac
+	;;
+	
+	# master 分支 (默认)
 	coolsnowwolf/lede:master)
 		cat >> ${Version_File} <<EOF
 sed -i '/check_signature/d' /etc/opkg.conf
@@ -93,13 +146,12 @@ then
 fi
 exit 0
 EOF
-		# sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${FEEDS_PKG}/ttyd/files/ttyd.config
-		# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-		# sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/argon-mod"' $(PKG_Finder d package default-settings)/files/zzz-default-settings
-		#sed -i "s?openwrt-23.05?master?g" ${FEEDS_CONF}
 		git reset --hard 1627fd2c745e496134834a8fb8145ba0aa458ae9
 		
-		rm -r ${FEEDS_LUCI}/luci-theme-argon*
+		# 移除argon主题（如果存在）
+		rm -r ${FEEDS_LUCI}/luci-theme-argon* 2>/dev/null || true
+		
+		# 添加核心插件
 		AddPackage other vernesong OpenClash dev
 		AddPackage other jerrykuku luci-app-argon-config master
 		AddPackage other sbwml luci-app-mosdns v5-lua
@@ -108,12 +160,15 @@ EOF
 		AddPackage msd_lite ximiTech luci-app-msd_lite main
 		AddPackage msd_lite ximiTech msd_lite main
 		AddPackage iptvhelper riverscn openwrt-iptvhelper master
-		rm -r ${FEEDS_PKG}/mosdns
-		rm -r ${FEEDS_LUCI}/luci-app-mosdns
-		rm -r ${FEEDS_PKG}/curl
-		rm -r ${FEEDS_PKG}/msd_lite
+		
+		# 移除冲突包
+		rm -r ${FEEDS_PKG}/mosdns 2>/dev/null || true
+		rm -r ${FEEDS_LUCI}/luci-app-mosdns 2>/dev/null || true
+		rm -r ${FEEDS_PKG}/curl 2>/dev/null || true
+		rm -r ${FEEDS_PKG}/msd_lite 2>/dev/null || true
 		Copy ${CustomFiles}/curl ${FEEDS_PKG}
 		
+		# ramips 优化
 		case "${TARGET_BOARD}" in
 		ramips)
 			sed -i "/DEVICE_COMPAT_VERSION := 1.1/d" target/linux/ramips/image/mt7621.mk
@@ -126,7 +181,7 @@ EOF
 			ClashDL mipsle-hardfloat tun
 		;;
 		esac
-			
+		
 		case "${TARGET_PROFILE}" in
 		d-team_newifi-d2)
 			Copy ${CustomFiles}/${TARGET_PROFILE}_system ${BASE_FILES}/etc/config system
@@ -137,6 +192,8 @@ EOF
 		;;
 		esac
 	;;
+	
+	# ImmortalWrt 分支
 	immortalwrt/immortalwrt*)
 		case "${TARGET_PROFILE}" in
 		x86_64)
@@ -145,26 +202,19 @@ EOF
 			x86_64)
 				AddPackage qosmate hudra0 qosmate main
 				AddPackage qosmate hudra0 luci-app-qosmate main
-				
 				AddPackage bandix timsaya luci-app-bandix main
 				AddPackage bandix timsaya openwrt-bandix main
-
 				AddPackage fakehttp yingziwu luci-app-fakehttp main
 				AddPackage fakehttp yingziwu openwrt-fakehttp main
-				
 				AddPackage passwall Openwrt-Passwall openwrt-passwall main
 			    AddPackage passwall Openwrt-Passwall openwrt-passwall-packages main
 				
 				git clone https://github.com/immortalwrt/packages /tmp/packages
 				git clone https://github.com/immortalwrt/luci /tmp/luci
-				#git clone https://github.com/QiuSimons/luci-app-daed package/dae
 				rm -rf feeds/packages/net/daed
 				rm -rf feeds/luci/applications/luci-app-daed
 				cp -a /tmp/packages/net/daed feeds/packages/net/daed
 				cp -a /tmp/luci/applications/luci-app-daed feeds/luci/applications/luci-app-daed
-				#cd package/dae
-				#git checkout e7040afc92a3bff4b9e4fca381e7e14a7be1b75e
-				#cd -
 				
 				sed -i 's/^local excluded_domain = {.*/local excluded_domain = {}/' package/passwall/openwrt-passwall/luci-app-passwall/root/usr/share/passwall/rule_update.lua
 				
@@ -181,13 +231,8 @@ EOF
 		;;
 		esac
 	;;
-	padavanonly/immortalwrtARM*)
-		case "${TARGET_PROFILE}" in
-		xiaomi_redmi-router-ax6s)
-			:
-		;;
-		esac
-	;;
+	
+	# MT798x 设备
 	hanwckf/immortalwrt-mt798x*)
 		case "${TARGET_PROFILE}" in
 		cmcc_rax3000m | jcg_q30)
@@ -216,6 +261,8 @@ EOF
 		esac
 	;;
 	esac
+	
+	# x86_64 通用优化
 	case "${TARGET_PROFILE}" in
 	x86_64)
 		Copy ${CustomFiles}/Depends/cpuset ${BASE_FILES}/bin
@@ -231,9 +278,6 @@ EOF
 		Copy /tmp/wstunnel ${BASE_FILES}/usr/bin
 		Copy /tmp/hysteria-linux-amd64 ${BASE_FILES}/usr/bin hysteria
 		chmod +x ${BASE_FILES}/usr/bin/hysteria ${BASE_FILES}/usr/bin/wstunnel
-
-		# ReleaseDL https://api.github.com/repos/Loyalsoldier/v2ray-rules-dat/releases/latest geosite.dat ${BASE_FILES}/usr/v2ray
-		# ReleaseDL https://api.github.com/repos/Loyalsoldier/v2ray-rules-dat/releases/latest geoip.dat ${BASE_FILES}/usr/v2ray
 	;;
 	esac
 }
